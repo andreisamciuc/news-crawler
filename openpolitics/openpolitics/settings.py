826199ec9f1a1ce8ceef25d78c4eee911f7580cd
Andrei Samciuc <andrei@sandd.ro>
2016-11-29 22:23:30 +0200

    zx

--- openpolitics/openpolitics/settings.py
+++ openpolitics/openpolitics/settings.py
@@ -25,7 +25,7 @@ LOG_ENABLED = False
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
@@ -70,7 +70,7 @@ CONCURRENT_REQUESTS = 32
 ITEM_PIPELINES = {
    'openpolitics.pipelines.MongoPipeline': 300
 }
-
+DOWNLOAD_DELAY = 0.25
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
---
+++ openpolitics/openpolitics/spiders/idnes_spider.py
@@ -0,0 +1,40 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+
+from openpolitics.items import OpenpoliticsItem
+import dateutil.parser
+
+class IdnesSpider(CrawlSpider):
+    name = 'idnes'
+    allowed_domains = ['idnes.cz']
+    start_urls = ['http://www.idnes.cz']
+    rules = (
+        # Sites which should be saved
+        Rule(
+            SgmlLinkExtractor(allow='(aspx\?c=A16|aspx\?c=A16)'),
+                # deny=('(komplettansicht|weitere|index)$', '/schlagworte/')),
+                callback='parse_page',
+                follow=True
+        ),
+
+        # Sites which should be followed, but not saved
+        Rule(SgmlLinkExtractor(allow='', deny='')),
+    )
+
+    def parse_page(self, response):
+        hxs = HtmlXPathSelector(response)
+        title = hxs.select('//meta[@property="og:title"]/@content').extract_first()
+        body = [s.strip() for s in hxs.select('//div[@class="text"]//p//text()').extract()]
+        time = hxs.select('//meta[@property="article:published_time"]/@content').extract_first()
+
+        if body:
+            item = OpenpoliticsItem()
+            item['title'] = title
+            item['text'] = body
+            item['url'] = response.url
+            if time:
+                item['date'] = dateutil.parser.parse(time)
+            item['i'] = 2
+
+            return item
