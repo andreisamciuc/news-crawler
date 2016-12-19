9de60064ae2d4f38419f51f51e9d8150c7f8b24e
Andrei Samciuc <andrei@sandd.ro>
2016-12-19 07:12:35 +0200

    xzc

---
+++ openpolitics/openpolitics/spiders/publico_spyder.py
@@ -0,0 +1,43 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+
+from openpolitics.items import OpenpoliticsItem
+
+import dateutil.parser
+
+class PublicoSpider(CrawlSpider):
+    name = 'publico'
+    allowed_domains = ['www.publico.pt']
+    start_urls = ['http://www.publico.pt']
+    cat_re = 'politica|mundo|economia'
+    rules = (
+        # Sites which should be saved
+        Rule(
+            SgmlLinkExtractor(allow='(%s)' % cat_re),
+                # deny=('(komplettansicht|weitere|index)$', '/schlagworte/')),
+                callback='parse_page',
+                follow=True
+        ),
+
+        # Sites which should be followed, but not saved
+        Rule(SgmlLinkExtractor(allow='(%s)' % cat_re, deny='(sport)')),
+    )
+
+    def parse_page(self, response):
+        hxs = HtmlXPathSelector(response)
+        title = hxs.select('//meta[@property="og:title"]/@content').extract_first()
+        body = [s.strip() for s in hxs.select('//div[@id="story-body"]//p//text()').extract()]
+        time = hxs.select('//time[@class="dateline"]/@datetime').extract_first()
+
+        if body:
+            item = OpenpoliticsItem()
+            item['title'] = title
+            item['text'] = body
+            item['url'] = response.url
+            # if time:
+            item['date'] = dateutil.parser.parse(time)
+            # item['time'] = time
+            item['i'] = 11
+
+            return item
---
+++ openpolitics/openpolitics/spiders/se_spyder.py
@@ -0,0 +1,47 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+
+from openpolitics.items import OpenpoliticsItem
+import dateutil.parser
+
+from simhash import Simhash
+
+class SeSpider(CrawlSpider):
+    name = 'se'
+    allowed_domains = ['www.se.pl']
+    start_urls = ['http://www.se.pl']
+    cat_re = 'polityka|swiat'
+    rules = (
+        # Sites which should be saved
+        Rule(
+            SgmlLinkExtractor(allow='(%s)' % cat_re),
+                # deny=('(komplettansicht|weitere|index)$', '/schlagworte/')),
+                callback='parse_page',
+                follow=True
+        ),
+
+        # Sites which should be followed, but not saved
+        Rule(SgmlLinkExtractor(allow='(%s)' % cat_re, deny='(sport)')),
+    )
+
+    def parse_page(self, response):
+        hxs = HtmlXPathSelector(response)
+        title = hxs.select('//meta[@property="og:title"]/@content').extract_first()
+        body = [s.strip() for s in hxs.select('//div[@class="article__body"]//p//text()').extract()]
+        time = hxs.select('//meta[@itemprop="datePublished"]/@content').extract_first()
+
+
+        if body:
+            item = OpenpoliticsItem()
+            item['title'] = title
+            item['text'] = body
+            item['url'] = response.url
+            if time:
+                item['date'] = dateutil.parser.parse(time)
+            else:
+                item['time'] = hxs.select('//div[@class="akt_bar"]/span/text()').extract()
+            # item['time'] = time
+            item['i'] = 10
+
+            return item
