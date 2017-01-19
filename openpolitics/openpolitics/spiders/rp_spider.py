fb12d0a43cfc0f94ace32fd0d071b9dccfc2b1b4
Andrei Samciuc <andrei@sandd.ro>
2017-01-19 10:36:46 +0200

    zx

--- openpolitics/openpolitics/spiders/rp_spider.py
+++ openpolitics/openpolitics/spiders/rp_spider.py
@@ -5,35 +5,34 @@ from scrapy.selector import HtmlXPathSelector
 from openpolitics.items import OpenpoliticsItem
 import dateutil.parser
 
-class BildSpider(CrawlSpider):
-    name = 'bild'
-    allowed_domains = ['bild.de']
-    start_urls = ['http://www.bild.de']
+class IdnesSpider(CrawlSpider):
+    name = 'rp'
+    allowed_domains = ['rp.pl']
+    start_urls = ['http://www.rp.pl']
     rules = (
         # Sites which should be saved
         Rule(
-            SgmlLinkExtractor(allow='(news|politik|geld)'),
+            SgmlLinkExtractor(allow='(/16|/17)'),
                 # deny=('(komplettansicht|weitere|index)$', '/schlagworte/')),
                 callback='parse_page',
                 follow=True
         ),
 
         # Sites which should be followed, but not saved
-        Rule(SgmlLinkExtractor(allow='', deny='suche/')),
+        Rule(SgmlLinkExtractor(allow='', deny='')),
     )
 
     def parse_page(self, response):
         hxs = HtmlXPathSelector(response)
-        title = hxs.select('//span[@class="headline"]//text()').extract_first().strip()
-        body = [s.strip() for s in hxs.select('//div[@class="txt"]/p//text()').extract()]
-        time = hxs.select('//div[@class="authors"]//time/@datetime').extract_first()
+        title = hxs.select('//meta[@property="og:title"]/@content').extract_first()
+        body = [s.strip() for s in hxs.select('//div[@class="article-content-text"]//p//text()').extract()]
+        time = hxs.select('//div[@class="article-datetime"]//@text()').extract_first()
 
         if body:
             item = OpenpoliticsItem()
             item['title'] = title
             item['text'] = body
             item['url'] = response.url
-            item['date'] = dateutil.parser.parse(time)
-            item['i'] = 0
+            item['time'] = time
 
             return item
