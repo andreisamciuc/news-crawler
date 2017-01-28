55b67a10b59484c0ead3c243b56325d07a9796e5
Andrei Samciuc <andrei@sandd.ro>
2017-01-28 14:55:26 +0200

    zx

--- openpolitics/openpolitics/spiders/novinky_spider.py
+++ openpolitics/openpolitics/spiders/novinky_spider.py
@@ -13,14 +13,14 @@ class NovinkySpider(CrawlSpider):
     rules = (
         # Sites which should be saved
         Rule(
-            LinkExtractor(allow=['ekonomika', 'domaci', 'zahranicni', 'archiv']),
+            LinkExtractor(allow=['ekonomika', 'domaci', 'zahranicni']),
                 # deny=('(komplettansicht|weitere|index)$', '/schlagworte/')),
                 callback='parse_page',
                 follow=True
         ),
 
         # Sites which should be followed, but not saved
-        Rule(LinkExtractor(allow='', deny='')),
+        Rule(LinkExtractor(allow=['ekonomika', 'domaci', 'zahranicni', 'archiv'], deny='')),
     )
 
     def parse_page(self, response):
